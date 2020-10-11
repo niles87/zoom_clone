0f2857f109c8c9aec558cec5be943000e3627b5c
Niles87 <nbing19@gmail.com>
2020-10-10 18:53:43 -0700

    added login and register forms

--- client/src/components/Login/index.tsx
+++ client/src/components/Login/index.tsx
@@ -1,3 +1,54 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import { existingUser } from "../../Interface/user";
+import Api from "../../API/API";
+import { Link } from "react-router-dom";
 
-export const Login = () => <h1>Login jsx</h1>;
+export const Login = () => {
+  const [formData, setFormData] = useState<existingUser>({
+    email: "",
+    password: "",
+  });
+
+  const submitForm = (ev: ChangeEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+    console.log(formData);
+    try {
+      const login = Api.login(formData);
+      console.log(login);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleInputChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = ev.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <>
+      <form onSubmit={submitForm}>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <p>
+        New to the site? Register <Link to="/register">here</Link>.
+      </p>
+    </>
+  );
+};
--- client/src/components/Register/index.tsx
+++ client/src/components/Register/index.tsx
@@ -1,3 +1,88 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import { user } from "../../Interface/user";
+import Api from "../../API/API";
+import { Link } from "react-router-dom";
 
-export const Register = () => <h1>Register jsx</h1>;
+export const Register = (props: any) => {
+  const [formData, setFormData] = useState<user>({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const submitForm = async (ev: ChangeEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+    console.log(formData);
+    try {
+      const user = await Api.signup(formData);
+      console.log(user);
+    } catch (error) {
+      console.error(error);
+    }
+    setFormData({
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      password: "",
+    });
+  };
+
+  const handleInputChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = ev.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <>
+      <form onSubmit={submitForm}>
+        <div>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="username"
+            placeholder="Display Name"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+      <p>
+        Already a member? <Link to="/">login</Link> here.
+      </p>
+    </>
+  );
+};
