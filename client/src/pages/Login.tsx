5911a638532d11aa7f6ebd9d7b35ae6d87e1ba99
Niles87 <nbing19@gmail.com>
2021-03-01 19:56:22 -0800

    updated login landing page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -6,7 +6,6 @@ const Container = styled.div`
     width: 90%;
     margin: 0 auto;
 `
-
 const Navbar = styled.ul`
     background: lightgrey;
     list-style-type: none;
@@ -15,12 +14,21 @@ const Navbar = styled.ul`
     width: 100%;
     display: flex;
     justify-content: space-between;
+    align-items: center;
 `
 
 const NavItem = styled.li`
-    padding: 5px;
+    padding: 0 20px;
 `
 
+const DropDown = styled.div<{ vis: string, dis: string }>`
+    padding-top: 3rem;
+    visibility: ${props => props.vis};
+    position: absolute;
+    right: 2%;
+    transition: all 0.5s ease;
+    display: ${props => props.dis};
+`
 
 export const LoginPage = () => {
     const [showLogin, setShowLogin] = useState<boolean>(false)
@@ -29,18 +37,28 @@ export const LoginPage = () => {
             <Container>
                 <Navbar>
                     <NavItem>
-                        <h1>
+                        <h2>
                             MOOZ
-                        </h1>
+                        </h2>
                     </NavItem>
-                    {showLogin ? (
-                        <NavItem>
-                            <Login />
-                        </NavItem>
-                    ) : ""}
-                    <button onClick={() => setShowLogin(!showLogin)}>{showLogin ? "Hide" : "Login"}</button>
+                    <NavItem>
+                        <button onClick={() => setShowLogin(!showLogin)}>{showLogin ? "Hide" : "Login"}</button>
+                        {showLogin ? (
+                            <DropDown vis={showLogin ? "visible" : "hidden"} dis={showLogin ? "block" : "none"}>
+                                <Login />
+                            </DropDown>
+                        ) : null}
+                    </NavItem>
                 </Navbar>
-
+                <div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum assumenda nihil odio omnis, in fugit! Nisi beatae obcaecati voluptatibus molestias cumque dolor enim delectus, odit dolorum quo reprehenderit eligendi, perspiciatis doloribus iusto tempora vero maiores voluptates porro quis libero harum labore. Beatae non hic fuga amet, suscipit illo quod doloremque sapiente perspiciatis incidunt recusandae laborum soluta corrupti ut. Minus vel voluptates pariatur laudantium dolorem consequatur eligendi tempora aliquam est reprehenderit ex porro deserunt ducimus cum possimus culpa debitis nobis quibusdam quod nemo, harum placeat nostrum. Ratione saepe ea laudantium quos? Laudantium nulla ad, doloremque earum modi at voluptatibus sapiente ipsa.
+                    </p>
+                    <img src="http://lorempixel.com/350/350" alt="placeholder" />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum assumenda nihil odio omnis, in fugit! Nisi beatae obcaecati voluptatibus molestias cumque dolor enim delectus, odit dolorum quo reprehenderit eligendi, perspiciatis doloribus iusto tempora vero maiores voluptates porro quis libero harum labore. Beatae non hic fuga amet, suscipit illo quod doloremque sapiente perspiciatis incidunt recusandae laborum soluta corrupti ut. Minus vel voluptates pariatur laudantium dolorem consequatur eligendi tempora aliquam est reprehenderit ex porro deserunt ducimus cum possimus culpa debitis nobis quibusdam quod nemo, harum placeat nostrum. Ratione saepe ea laudantium quos? Laudantium nulla ad, doloremque earum modi at voluptatibus sapiente ipsa.
+                    </p>
+                </div>
             </Container>
         </Fragment>
     )
