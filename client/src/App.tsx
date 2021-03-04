e7360a31c97355128361cca3e42c5d4e3b0f0bf2
Niles87 <nbing19@gmail.com>
2021-03-03 19:35:18 -0800

    removed register from router its now included in landing, renamed import of Login to landing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { LoginPage } from "./pages/Login";
-import { Register } from "./components/Register";
+import { LandingPage } from "./pages/Landing";
 import { Room } from "./components/Room";
 import { Home } from "./pages/Home";
 import "./App.css";
@@ -11,8 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <Switch>
-          <Route exact path="/" component={LoginPage} />
-          <Route exact path="/register" component={Register} />
+          <Route exact path="/" component={LandingPage} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/room/:id" component={Room} />
         </Switch>
