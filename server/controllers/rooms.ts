7f326452d8f2d11f6063a009e4195fad1b79d111
Niles87 <nbing19@gmail.com>
2021-03-09 18:28:09 -0800

    updated, added and tested new routes and room controller

--- server/controllers/rooms.ts
+++ server/controllers/rooms.ts
@@ -3,17 +3,28 @@ import Room, { IRoomModel } from "../model/Room";
 
 export const rooms = {
   seeRooms: function (req: Request, res: Response) {
-    Room.find({}).then((rooms) => res.json(rooms));
+    Room.find({}).select("-__v").then((rooms) => res.json(rooms));
   },
   create: function (req: Request, res: Response) {
     const { owner } = req.body;
     Room.create({ roomOwner: owner, members: [] })
       .then((room: IRoomModel) => {
-        res.sendStatus(201);
+        res.sendStatus(201).json(room);
       })
       .catch((err: any) => {
         console.error(err.message);
         res.sendStatus(418);
       });
   },
+  addMemberToRoom: async function (req: Request, res: Response) {
+    const { id, member } = req.body
+    try {
+      const room = await Room.findByIdAndUpdate({ _id: id }, {
+        $push: { members: member._id }
+      }, { new: true }).select("-__v")
+      res.json(room)
+    } catch (error) {
+      res.sendStatus(404)
+    }
+  }
 };
--- server/routes/room.ts
+++ server/routes/room.ts
@@ -4,5 +4,6 @@ import { rooms } from "../controllers/rooms";
 const room = router();
 room.route("/").get(rooms.seeRooms);
 room.route("/createRoom").post(rooms.create);
+room.route("/addMember").post(rooms.addMemberToRoom)
 
 export default room;
