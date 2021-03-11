49791c837a38bae57da93179bf7b853b4cb1b65e
Niles87 <nbing19@gmail.com>
2021-03-10 20:34:00 -0800

    added more functionality to create page, added route to get to rooms

--- client/src/components/Create/index.tsx
+++ client/src/components/Create/index.tsx
@@ -1,8 +1,34 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Modal, MButton } from "../Modal";
+import { user } from "../../Interface/user";
+import { Toast } from "../Layout";
+import Api from "../../API";
+import { Link } from "react-router-dom";
 
-export const Create = (props: any) => {
+interface createProps {
+  info: user
+}
+interface Irooms {
+  members: string[];
+  roomOwner: string
+  _id: string
+}
+export const Create = (props: createProps) => {
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [rooms, setRooms] = useState<Irooms[]>([])
+  const [toast, showToast] = useState<boolean>(false)
+  const [message, setMessage] = useState<string>("")
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("toasting ;)")
+      showToast(false)
+    }, 2000)
+  }, [toast])
+
+  useEffect(() => {
+    getRooms()
+  }, [])
 
   const closeModal = () => {
     setShowModal(false);
@@ -11,16 +37,47 @@ export const Create = (props: any) => {
   const openModal = () => {
     setShowModal(true);
   };
+
+  const getRooms = async () => {
+    let list: Irooms[] = []
+    try {
+      list = await Api.viewRooms().then(res => res.json())
+
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setRooms(prev => [...prev, ...list])
+    }
+  }
+
+  const openRoom = async (name: string) => {
+    try {
+      const room = await Api.createRoom(name).then(async (res: Response) => {
+        return await res.json()
+      })
+      if (room) {
+        setRooms(prevState => [...prevState, room])
+      }
+    } catch (err) {
+      setShowModal(false)
+      setMessage("Failed to Create Room")
+      showToast(true)
+    }
+  }
+
   return (
     <Fragment>
+      <Toast dis={toast}>{message}</Toast>
       <Modal show={showModal} handleClose={closeModal}>
         <div>
-          <MButton>Create a new Room</MButton>
+          <MButton onClick={() => openRoom(props.info.username)}>Create a new Room</MButton>
         </div>
         <div>
           <p>Did you mean to join a room?</p>
           <ul>
-            <li>rooms</li>
+            {rooms.length > 0 ? (rooms.map((room: Irooms) => (
+              <li key={room._id}><Link to={`/room/${room._id}`}>{room.roomOwner}</Link></li>
+            ))) : (<li>no rooms</li>)}
           </ul>
         </div>
       </Modal>
