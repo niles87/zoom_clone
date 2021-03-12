90d83826dcdb377553ae0838c182db64474332d6
Niles87 <nbing19@gmail.com>
2021-03-11 21:45:08 -0800

    added styles to components

--- client/src/components/Video/index.tsx
+++ client/src/components/Video/index.tsx
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
-export const Video = styled.video`
+export const VideoContainer = styled.div<{ talking: boolean }>`
+    width: ${props => props.talking ? "500px" : "150px"};
+    height: ${props => props.talking ? "375px" : "113px"};
+    border: ${props => props.talking ? "3px solid rgb(59,209,19)" : "3px solid rgb(33,33,33)"};
+    overflow: hidden;
+`
 
+export const Video = styled.video`
+    height: 100%;
 `
