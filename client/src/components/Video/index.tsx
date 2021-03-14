ab8db7dc6885395817599b63848fb3acf505436a
Niles87 <nbing19@gmail.com>
2021-03-13 18:18:59 -0800

    added a username label to videos

--- client/src/components/Video/index.tsx
+++ client/src/components/Video/index.tsx
@@ -5,8 +5,19 @@ export const VideoContainer = styled.div<{ talking: boolean }>`
     height: ${props => props.talking ? "375px" : "113px"};
     border: ${props => props.talking ? "3px solid rgb(59,209,19)" : "3px solid rgb(33,33,33)"};
     overflow: hidden;
+    position: relative;
+`
+
+export const Name = styled.span<{ talking: boolean }>`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    font-size: ${props => props.talking ? "15px" : "10px"};
+    color: rgb(244,244,244);
+    z-index: 5;
 `
 
 export const Video = styled.video`
     height: 100%;
+    z-index: -1;
 `
